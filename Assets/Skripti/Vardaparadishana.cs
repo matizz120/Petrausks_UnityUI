59c7d720ddaca4f4cc73f0e75c790d86d38f7756
Matīss Petrausks <matiss44555@gmail.com>
2021-03-19 05:40:38 +0200

    Projektu uzsākšana/pievienots tekstu lauks

---
+++ Assets/Skripti/Vardaparadishana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vardaparadishana : MonoBehaviour
+{
+    public string vards;
+    public string vecums;
+    public GameObject ievadteksts1;
+    public GameObject ievadteksts2;
+    public GameObject tekstuAtteeloshana;
+     public void uzglabaatTekstuGadu(){
+        vards = ievadteksts1.GetComponent<Text>().text;
+        vecums = ievadteksts2.GetComponent<Text>().text;
+        tekstuAtteeloshana.GetComponent<Text>().text = vards.ToUpper()+" ir "+vecums.ToUpper()+" gadus vecs!";
+    }
+}
